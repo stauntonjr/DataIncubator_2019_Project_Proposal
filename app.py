273c5b8a87ebe1803f4646555fb87473ea86a45e
stauntonjr <36486177+stauntonjr@users.noreply.github.com>
2019-05-20 16:20:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import numpy as np
 
-# coverage chart data
+########### Set up the chart
 year = np.linspace(2013, 1, 2017)
 
 pubcov_blk = np.array([40.7,42.3,43.5,44.0,43.7])
@@ -20,107 +20,107 @@ anycov_non = np.array([85.7,88.4,90.6,91.4,91.2])
 anycov_tot = np.array([85.2,88.0,90.3,91.1,91.0])
 
 # Create traces
-trace_pub_blk = go.Scatter(
+trace_any_tot = go.Scatter(
   x = year,
-  y = pubcov_blk
-  #mode = 'lines+markers'
-  name = 'Black (public)'
-  line = dict(
-    color = ('rgb(205, 12, 24)'),
-    width = 4,
-    dash = 'dashdot')
-)
-trace_pub_non = go.Scatter(
-  x = year,
-  y = pubcov_non
-  #mode = 'lines+markers'
-  name = 'Non-Black (public)'
-  line = dict(
-    color = ('rgb(205, 12, 24)'),
-    width = 4,
-    dash = 'dash')
-)
-trace_pub_tot = go.Scatter(
-  x = year,
-  y = pubcov_tot
-  #mode = 'lines+markers'
-  name = 'All (public)'
-  line = dict(
-    color = ('rgb(205, 12, 24)'),
-    width = 4,)
-)
-
-trace_prv_blk = go.Scatter(
-  x = year,
-  y = prvcov_blk
-  #mode = 'lines+markers'
-  name = 'Black (private)'
-  line = dict(
-    color = ('rgb(22, 96, 167)'),
-    width = 4,
-    dash = 'dashdot')
-)
-trace_prv_non = go.Scatter(
-  x = year,
-  y = prvcov_non
-  #mode = 'lines+markers'
-  name = 'Non-Black (private)'
-  line = dict(
-    color = ('rgb(22, 96, 167)'),
-    width = 4,
-    dash = 'dash')
-)
-trace_prv_tot = go.Scatter(
-  x = year,
-  y = prvcov_tot
-  #mode = 'lines+markers'
-  name = 'All (private)'
-  line = dict(
-    color = ('rgb(22, 96, 167)'),
-    width = 4,)
-)
-
-trace_any_blk = go.Scatter(
-  x = year,
-  y = anycov_blk
-  #mode = 'lines+markers'
-  name = 'Black (any)'
+  y = anycov_tot,
+  mode = 'lines+markers',
+  name = 'Any (All)',
   line = dict(
     color = ('rgb(0, 0, 0)'),
-    width = 4,
+    width = 2,)
+)
+trace_any_blk = go.Scatter(
+  x = year,
+  y = anycov_blk,
+  mode = 'lines+markers',
+  name = 'Any (Black)',
+  line = dict(
+    color = ('rgb(0, 0, 0)'),
+    width = 2,
     dash = 'dashdot')
 )
 trace_any_non = go.Scatter(
   x = year,
-  y = anycov_non
-  #mode = 'lines+markers'
-  name = 'Non-Black (any)'
+  y = anycov_non,
+  mode = 'lines+markers',
+  name = 'Any (Non-Black)',
   line = dict(
     color = ('rgb(0, 0, 0)'),
-    width = 4,
+    width = 2,
     dash = 'dash')
 )
-trace_any_tot = go.Scatter(
+
+trace_prv_tot = go.Scatter(
   x = year,
-  y = anycov_tot
-  #mode = 'lines+markers'
-  name = 'All (any)'
+  y = prvcov_tot,
+  mode = 'lines+markers',
+  name = 'Private (All)',
   line = dict(
-    color = ('rgb(0, 0, 0)'),
-    width = 4,)
+    color = ('rgb(22, 96, 167)'),
+    width = 2,)
 )
-data = [trace_pub_blk, trace_pub_non, trace_pub_tot, 
-            trace_prv_blk, trace_prv_non, trace_prv_tot,
-            trace_any_blk, trace_any_non, trace_any_tot]
+trace_prv_blk = go.Scatter(
+  x = year,
+  y = prvcov_blk,
+  mode = 'lines+markers',
+  name = 'Private (Black)',
+  line = dict(
+    color = ('rgb(22, 96, 167)'),
+    width = 2,
+    dash = 'dashdot')
+)
+trace_prv_non = go.Scatter(
+  x = year,
+  y = prvcov_non,
+  mode = 'lines+markers',
+  name = 'Private (Non-Black)',
+  line = dict(
+    color = ('rgb(22, 96, 167)'),
+    width = 2,
+    dash = 'dash')
+)
+
+trace_pub_tot = go.Scatter(
+  x = year,
+  y = pubcov_tot,
+  mode = 'lines+markers',
+  name = 'Public (All)',
+  line = dict(
+    color = ('rgb(205, 12, 24)'),
+    width = 2,)
+)
+trace_pub_blk = go.Scatter(
+  x = year,
+  y = pubcov_blk,
+  mode = 'lines+markers',
+  name = 'Public (Black)',
+  line = dict(
+    color = ('rgb(205, 12, 24)'),
+    width = 2,
+    dash = 'dashdot')
+)
+trace_pub_non = go.Scatter(
+  x = year,
+  y = pubcov_non,
+  mode = 'lines+markers',
+  name = 'Public (Non-Black)',
+  line = dict(
+    color = ('rgb(205, 12, 24)'),
+    width = 2,
+    dash = 'dash')
+)
+
+census_data = [trace_any_tot, trace_any_blk, trace_any_non,
+               trace_prv_tot, trace_prv_blk, trace_prv_non,
+               trace_pub_tot, trace_pub_blk, trace_pub_non]
 
 # Edit layout
-layout = dict(title = 'Disparity in Health Insurance Coverage by Race',
+census_layout = dict(title = 'Disparity in Health Insurance Coverage by Race',
               xaxis = dict(title = 'Year'),
               yaxis = dict(title = 'Insured (% National Population)'),
               )
               
-fig = dict(data=data, layout=layout)
-py.iplot(fig, filename='coverage')
+census_fig = go.Figure(data=census_data, layout=census_layout)
 
 ########### Display the chart
 
@@ -128,11 +128,15 @@ app = dash.Dash()
 server = app.server
 
 app.layout = html.Div(children=[
-    html.H1('Census Data'),
+    html.H1('Health Insurance Coverage Disparity'),
     dcc.Graph(
-        id='censusdata',
-        figure=fig
-    )]
+        id='census',
+        figure=census_fig
+    ),
+    html.A('Code on Github', href='https://github.com/stauntonjr/TDI_2019_Project_Proposal'),
+    html.Br(),
+    html.A('Data Source', href='https://www.census.gov/programs-surveys/acs/data/pums.html'),
+    ]
 )
 
 if __name__ == '__main__':
